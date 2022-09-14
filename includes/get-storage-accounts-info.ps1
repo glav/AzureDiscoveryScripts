cbcc2c56c6181b98553adfefa386c0e7d7ce2f03
Paul Glavich <glav@theglavs.com>
2022-09-15 09:58:11 +1000

    Added some storage account checks

--- includes/get-storage-accounts-info.ps1
+++ includes/get-storage-accounts-info.ps1
@@ -4,15 +4,25 @@ function GetStorageAccountsInfo {
     ) 
 
     Write-Host "Retrieving Storage accounts information and configuration..."
-    # $jsonResult = az advisor recommendation list --query '[].{sku: extendedProperties.displaySKU, category:category, impact:impact, problem: shortDescription.problem, annualSavings: extendedProperties.annualSavingsAmount, currency: extendedProperties.savingsCurrency, resourceGroup: resourceGroup, resourceType: impactedField, name: impactedValue}' | ConvertFrom-Json
+    $jsonResult = az storage account list --query '[].{publicNetworkAccess: publicNetworkAccess, allowBlobPublicAccess:allowBlobPublicAccess, name:name, minimumTlsVersion:minimumTlsVersion, resourceGroup:resourceGroup, resourceType:type, enableHttpsTrafficOnly:enableHttpsTrafficOnly}' | ConvertFrom-Json
 
-    # Foreach ($item IN $jsonResult) {
-    #     if ($nul -ne $item.resourceGroup -and $nul -ne $item.resourceType -and $nul -ne $item.name) {
-    #         # $itemDetail = az resource show -g $item.resourceGroup -n $item.name --resource-type $item.resourceType | ConvertFrom-Json
-    #         # $item.itemDetail = $itemDetail
-    #         $modelItem = DefineCommonModel -Pillar 'pillar' -ResourceType $item.resourceType -ResourceGroup $item.resourceGroup -ResourceName $item.name -Severity $item.impact -Issue $item.problem
+    Foreach ($item IN $jsonResult) {
+        if ($nul -ne $item.resourceGroup -and $nul -ne $item.resourceType -and $nul -ne $item.name) {
+            # $itemDetail = az resource show -g $item.resourceGroup -n $item.name --resource-type $item.resourceType | ConvertFrom-Json
+            # $item.itemDetail = $itemDetail
+
+            # Is public network access enabled?
+            if ($item.publicNetworkAccess -eq 'Enabled') {
+                [void]$resultsArray.Add($(DefineCommonModel -Pillar 'Security' -ResourceType $item.resourceType -ResourceGroup $item.resourceGroup -ResourceName $item.name -Severity 'High' -Issue 'Public network access ENABLED'))    
+            }
+
+            # Is blob public access enabled?
+            if ($item.allowBlobPublicAccess -eq $True) {
+                [void]$resultsArray.Add($(DefineCommonModel -Pillar 'Security' -ResourceType $item.resourceType -ResourceGroup $item.resourceGroup -ResourceName $item.name -Severity 'High' -Issue 'Blob Public access ENABLED'))    
+            }
+            #$modelItem = DefineCommonModel -Pillar 'pillar' -ResourceType $item.resourceType -ResourceGroup $item.resourceGroup -ResourceName $item.name -Severity $item.impact -Issue $item.problem
         
-    #         [void]$resultsArray.Add($modelItem)
-    #     }
-    # }
+            #[void]$resultsArray.Add($modelItem)
+        }
+    }
 }
